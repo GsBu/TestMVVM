2e13ad661ebcbc1646e0c9a4d5a8daadf9b9104e
longlong <longlong.bu@camdora.me>
2017-11-20 16:52:57 +0800

    增加测试代码

--- TestMVVM/app/src/main/java/com/example/camdora/testmvvm/MainActivity.java
+++ TestMVVM/app/src/main/java/com/example/camdora/testmvvm/MainActivity.java
@@ -1,13 +1,24 @@
 package com.example.camdora.testmvvm;
 
+import android.databinding.DataBindingUtil;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.example.camdora.testmvvm.bean.User;
+import com.example.camdora.testmvvm.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        ActivityMainBinding binding = DataBindingUtil
+                .setContentView(this, R.layout.activity_main);
+
+        User user1 = new User("Gs", 24);
+        binding.setUser2(user1);
+
+        User user4 = new User("pxy", 26);
+        binding.setUser3(user4);
     }
 }

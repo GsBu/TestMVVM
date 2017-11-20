72cabc0e13334edac964b5073536180ce9943639
longlong <longlong.bu@camdora.me>
2017-11-20 17:45:32 +0800

    增加Observable，实现自动更新UI

--- TestMVVM/app/src/main/java/com/example/camdora/testmvvm/MainActivity.java
+++ TestMVVM/app/src/main/java/com/example/camdora/testmvvm/MainActivity.java
@@ -3,22 +3,46 @@ package com.example.camdora.testmvvm;
 import android.databinding.DataBindingUtil;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 import com.example.camdora.testmvvm.bean.User;
 import com.example.camdora.testmvvm.databinding.ActivityMainBinding;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+        implements View.OnClickListener{
+    private static final String TAG = "MainActivity";
+
+    private ActivityMainBinding binding;
+    private User user1, user4;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        ActivityMainBinding binding = DataBindingUtil
+        binding = DataBindingUtil
                 .setContentView(this, R.layout.activity_main);
 
-        User user1 = new User("Gs", 24);
+        user1 = new User("Gs", 24);
         binding.setUser2(user1);
 
-        User user4 = new User("pxy", 26);
+        user4 = new User("pxy", 26);
         binding.setUser3(user4);
+
+        binding.setClickListener1(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.bt_1:
+                if(user1 != null && user4 != null){
+                    user1.setName("Gs1993");
+                    user4.setName("pxy1991");
+                    Log.e(TAG, "改变user值");
+                }
+                break;
+            default:
+                break;
+        }
     }
 }
--- TestMVVM/app/src/main/java/com/example/camdora/testmvvm/bean/User.java
+++ TestMVVM/app/src/main/java/com/example/camdora/testmvvm/bean/User.java
@@ -1,10 +1,15 @@
 package com.example.camdora.testmvvm.bean;
 
+import android.databinding.BaseObservable;
+import android.databinding.Bindable;
+
+import com.android.databinding.library.baseAdapters.BR;
+
 /**
  * Created by camdora on 17-11-20.
  */
 
-public class User {
+public class User extends BaseObservable {
     private String name;
     private int age;
 
@@ -17,19 +22,23 @@ public class User {
         this.age = age;
     }
 
+    @Bindable
     public String getName() {
         return name;
     }
 
     public void setName(String name) {
         this.name = name;
+        notifyPropertyChanged(BR.name);
     }
 
+    @Bindable
     public int getAge() {
         return age;
     }
 
     public void setAge(int age) {
         this.age = age;
+        notifyPropertyChanged(BR.age);
     }
 }
